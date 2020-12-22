652de66ce0ac284747956f33048d9901ae56df72
Bas Kuis <b@ukora.com>
2020-12-22 12:01:27 -0600

    collections

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,14 +1,14 @@
-import classes.Banana;
-import classes.Kiwi;
+import classes.*;
 import examples.RegexExample;
 
 import java.io.IOException;
-import java.util.Random;
-import java.util.UUID;
+import java.util.*;
 
 public class App {
 
     public static void main(String[] args) throws InterruptedException, IOException {
+
+
 
 
     }
@@ -16,6 +16,59 @@ public class App {
 
 
 
+    static void collectionsExamples() {
+        // Queue of cars at carwash
+        Stack<Car> carsInGarage = new Stack<Car>();
+        carsInGarage.add(new Toyota());
+        carsInGarage.add(new Toyota());
+        carsInGarage.add(new Toyota());
+        carsInGarage.add(new Tesla());
+
+        Car easyToTakeFromGarage = carsInGarage.pop();
+
+        System.out.println("The easiest to take out of garage electric: " + easyToTakeFromGarage.isElectric());
+
+        // A Toyota
+        // B Tesla
+
+        // Queue of cars at carwash
+        Queue<Car> carsQueueAtCarwash = new LinkedList<Car>();
+        carsQueueAtCarwash.add(new Toyota());
+        carsQueueAtCarwash.add(new Toyota());
+        carsQueueAtCarwash.add(new Toyota());
+        carsQueueAtCarwash.add(new Tesla());
+
+        Car nextInLine = carsQueueAtCarwash.remove();
+
+        System.out.println("Is next in line at carwash electric: " + nextInLine.isElectric());
+        // A: That is a toyota
+        // B: That is a tesla
+
+
+
+        // Are fixed in size
+        // Awkward for most cases
+        Car[] carsArray = new Car[10];
+        carsArray[0] = new Tesla();
+        carsArray[1] = new Toyota();
+        for (int i = 0; i < carsArray.length; i++){
+            if (carsArray[i] != null) {
+                System.out.println(carsArray[i].isElectric());
+            } else {
+                System.out.println("No car at entry " + i);
+            }
+        }
+
+        // Read more natural
+        // More flexible
+        List<Car> carsList = new ArrayList<Car>();
+        carsList.add(new Tesla());
+        carsList.add(new Toyota());
+
+        for (int i = 0; i < carsList.size(); i++){
+            System.out.println(carsList.get(i).isElectric());
+        }
+    }
 
 
 
