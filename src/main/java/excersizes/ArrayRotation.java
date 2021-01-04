3bfee128cd71e32cefea3167a80a73e66ab84aeb
Bas Kuis <b@ukora.com>
2021-01-04 11:48:29 -0600

    array rotation notes

--- src/main/java/excersizes/ArrayRotation.java
+++ src/main/java/excersizes/ArrayRotation.java
@@ -4,7 +4,38 @@ public class ArrayRotation {
 
     public static void rotateArray() {
 
-//        String[] items =
+        String[] items = {
+                "tree",
+                "house",
+                "boat",
+                "cup",
+                "water"
+        };
+        String[] rotatedItems = new String[items.length];
+
+        int rotateBy = 11;
+
+        for (int i = 0; i < items.length; i++) {
+            String item = items[i];
+            int newIndex = i + rotateBy;
+            newIndex = newIndex % items.length;
+//            if (newIndex > items.length - 1) {
+//                newIndex = newIndex - items.length;
+//            }
+            rotatedItems[newIndex] = item;
+        }
+
+        System.out.println("original array ");
+        for (String original : items) {
+            System.out.println(original);
+        }
+
+        System.out.println("============");
+
+        System.out.println("rotated by " + rotateBy);
+        for (String rotated : rotatedItems) {
+            System.out.println(rotated);
+        }
 
         /**
          * [0] -- tree
