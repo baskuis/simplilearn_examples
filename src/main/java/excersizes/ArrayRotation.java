2981c194877f78842ff871d22d9e464779bebbde
Bas Kuis <b@ukora.com>
2021-01-04 11:09:56 -0600

    array rotation class

---
+++ src/main/java/excersizes/ArrayRotation.java
@@ -0,0 +1,24 @@
+package excersizes;
+
+public class ArrayRotation {
+
+    public static void rotateArray() {
+
+//        String[] items =
+
+        /**
+         * [0] -- tree
+         * [1] -- house
+         * [2] -- boat
+         * [3] -- cup
+         * [4] -- water
+         *
+         * We want 'house' to be the first entry in the array
+         * - tree will have to move to position 4
+         */
+
+        System.out.println("Hi - ready to go");
+
+    }
+
+}
