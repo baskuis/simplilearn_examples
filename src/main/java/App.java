01869b277fd6973413349f8b73516e0c78d7a61f
Bas Kuis <b@ukora.com>
2021-01-06 13:00:08 -0600

    search problems

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -31,6 +31,9 @@ public class App {
 
     public static void main(String[] args) throws IOException {
 
+    }
+
+    static void searchProblems() {
         int[] numbers = {1,2,3,4,5,6,7,8,9,10,11,12,13,15,16,17,18,19,20,21,22,23};
         int lookingFor = 10;
 
